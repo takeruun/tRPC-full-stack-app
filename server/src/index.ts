7e75d481236dff8fdfe2345c0bd708ac0229729f
takeru.hashimoto <hstake25@gmail.com>
2023-04-28 09:13:53 +0900

    tRPC server

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import express from 'express'
+
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { appRouter } from './router';
+import { createContext } from './context';
+
+const app: express.Express = express();
+
+app.use(cors());
+
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+
+app.listen(3210);
---
+++ server/src/user/db.ts
@@ -0,0 +1,6 @@
+import { User } from './types';
+
+export const users: User[] = [
+  { id: '1', name: 'John' },
+  { id: '2', name: 'Jane' },
+];
